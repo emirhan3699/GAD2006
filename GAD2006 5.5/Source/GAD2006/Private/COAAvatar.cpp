52ed35abcd1305708413721ce10f76b5636c10e3
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-11-20 18:44:40 +0300

    a
    a

---
+++ GAD2006 5.5/Source/GAD2006/Private/COAAvatar.cpp
@@ -0,0 +1,162 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "COAAvatar.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ACOAAvatar::ACOAAvatar() :
+	RunSpeed(800),
+	Stamina(100),
+	MaxStamina(100),
+	SGR(7),
+	SDR(25),
+	SRR(7),
+	bStaminaDrained(false),
+	bRunning(false)
+
+{
+	mSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	mSpringArm->TargetArmLength = 400.0f;
+	mSpringArm->SetupAttachment(RootComponent);
+
+	mCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("MainCamera"));
+	mCamera->SetupAttachment(mSpringArm, USpringArmComponent::SocketName);
+}
+
+void ACOAAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+	mCamera->bUsePawnControlRotation = false;
+	mSpringArm->bUsePawnControlRotation = true;
+	bUseControllerRotationYaw = false;
+}
+
+
+
+
+// Called to bind functionality to input
+void ACOAAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ACharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ACharacter::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACOAAvatar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACOAAvatar::MoveRight);
+
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Released, this, &ACOAAvatar::StopJumping);
+
+	PlayerInputComponent->BindAction("Run", EInputEvent::IE_Pressed, this, &ACOAAvatar::RunPressed);
+	PlayerInputComponent->BindAction("Run", EInputEvent::IE_Released, this, &ACOAAvatar::RunReleased);
+
+
+}
+
+void ACOAAvatar::RunPressed()
+{
+	// Koşabiliyorsa
+	if (Stamina > 0.0f && !bStaminaDrained)
+	{
+		bRunning = true;
+		UpdateMovement();
+	}
+}
+
+void ACOAAvatar::RunReleased()
+{
+	// Koşmuyorsa
+	if (bRunning)
+	{
+		bRunning = false;
+		UpdateMovement();
+	}
+}
+
+
+void ACOAAvatar::MoveForward(float value)
+{
+	FRotator Rotation = GetController()->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+	FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, value);
+	GEngine->AddOnScreenDebugMessage(0, 1, FColor::Cyan, FString::Printf(TEXT("Move Forward: %f"), value));
+}
+
+void ACOAAvatar::MoveRight(float value)
+{
+	FRotator Rotation = GetController()->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+	FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirection, value);
+
+}
+void ACOAAvatar::Tick(float DeltaTime)
+{
+	// Stamina kontrolü
+	if (bRunning && !bStaminaDrained)
+	{
+		if (GetCharacterMovement()->MovementMode == MOVE_Walking)
+		{
+			Stamina -= SDR * DeltaTime;
+
+			if (Stamina <= 0.0f)
+			{
+				bRunning = false;
+				bStaminaDrained = true;
+				Stamina = 0.0f;  // Stamina'nın negatif değerlere düşmesini engelle
+				UpdateMovement();
+			}
+		}
+	}
+	else
+	{
+		if (Stamina < MaxStamina && SRR > 0.0f)
+		{
+			Stamina += SRR * DeltaTime;
+
+			// Stamina maksimum seviyeye ulaşırsa normale döner
+			if (Stamina >= MaxStamina)
+			{
+				bStaminaDrained = false;
+				Stamina = MaxStamina;
+			}
+		}
+	}
+	Super::Tick(DeltaTime);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, DeltaTime, FColor::Green, FString::Printf(TEXT("Stamina: %f"), Stamina));
+	}
+	
+
+}
+void ACOAAvatar::UpdateMovement()
+{
+	if (!bStaminaDrained)
+	{
+		if (bRunning)
+		{
+			GetCharacterMovement()->MaxWalkSpeed = RunSpeed;
+		}
+		else
+		{
+			GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+		}
+	}
+	else
+	{
+		GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+	}
+
+}
+
+
+
+
+
+
+
+
