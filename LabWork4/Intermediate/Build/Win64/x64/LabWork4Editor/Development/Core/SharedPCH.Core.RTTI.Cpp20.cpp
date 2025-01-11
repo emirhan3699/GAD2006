1633645ad619718e0d37b6bc8b8f3d0f9562f6ce
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2025-01-11 15:56:44 +0300

    deleted

--- LabWork4/Intermediate/Build/Win64/x64/LabWork4Editor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32546
-#include "C:/Users/EnesAkar/Desktop/LabWork4/Intermediate/Build/Win64/x64/LabWork4Editor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
--- LabWork4/Intermediate/Build/Win64/x64/LabWork4Editor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/EnesAkar/Desktop/LabWork4/Intermediate/Build/Win64/x64/LabWork4Editor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
